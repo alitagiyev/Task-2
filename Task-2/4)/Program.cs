0557ab074be233aa23cb049e632cfdcdfd912239
alitagiyev <alint@code.edu.az>
2023-03-05 17:06:45 +0400

    3

--- Task-2/4)/Program.cs
+++ Task-2/4)/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+        /*verilmihs 5 reqemli
+         * ilk ve son reqemlerinin 
+         * ceminin
+         * kvadrati
+         */
+        l1:
+            Console.Write("5 reqemli eded daxil edin: ");
+
+            if (!int.TryParse(Console.ReadLine(), out int a) || a < 9999 || a > 100000)
+            {
+
+                goto l1;
+            }
+
+         
         }
     }
 }
---
+++ Task-3/1)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace _1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "";
+
+            string surname = "";
+
+            string age = "";
+
+            string speciality = "";
+
+            string group = "";
+
+        }
+        Console.Write(name);
+        Console.Write(surname);
+        Console.Write(age);
+        Console.Write(speciality);
+        Console.Write(group);
+    }
+}
