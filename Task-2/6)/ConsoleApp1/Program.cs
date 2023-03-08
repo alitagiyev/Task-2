0ed357f22fad9b5d0365bb73db9f4ce65b5a980a
alitagiyev <alint@code.edu.az>
2023-03-09 03:29:08 +0400

    done

---
+++ Task-2/6)/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        //6) verilmihs 8 reqemli ededin ilk I ve axirinci reqemlerini legv et
+        l1:
+            Console.WriteLine("8 reqemli eded daxil edin:");
+
+            string number1 = Console.ReadLine();
+
+            int number;
+
+            if (!int.TryParse(number1, out number))
+            {
+                goto l1;
+            }
+            int len = (int)Math.Log10(number) + 1;
+            if (len != 8)
+            {
+                goto l1;
+            }
+            number = number % 10000000;
+            number = number /  10;
+
+            Console.WriteLine(number);
+
+        }
+    }
+}
