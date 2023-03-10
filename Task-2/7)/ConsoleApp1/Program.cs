83c6365ae6a27ab8577a6afc5b40fdb31fddcff8
alitagiyev <alint@code.edu.az>
2023-03-11 03:02:02 +0400

    done

---
+++ Task-2/7)/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        //    7) verilmihs 4 reqemli ededin tersine duzub axirina ve evveline 8 artir
+        l1:
+            Console.Write("4 reqemli eded daxil edin:");
+
+            string number1 = Console.ReadLine();
+
+            int number;
+
+            if (!int.TryParse(number1, out number))
+            {
+                goto l1;
+            }
+            int len = (int)Math.Log10(number) + 1;
+            if (len != 4)
+            {
+                goto l1;
+            }
+            int qaliq;
+            int newNum=0;
+            while (number != 4)
+            {
+                qaliq = number % 10;
+                newNum = newNum * 10 + qaliq;
+                number /= 10;
+            }
+            Console.Write(newNum);
+        }
+    }
+}
---
+++ Task-2/8)/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //8) Verilmis ededdin axirdan 3 - cu reqemi ile sonuncu reqeminin cemini tap
+            int number = 345678;
+            
+
+            int LastDigit = number % 10;
+
+            number = number / 100;
+
+            int ThirdDigit = number % 10;
+
+
+           
+            int cem = LastDigit + ThirdDigit;
+
+            Console.WriteLine(cem);
+        }
+    }
+}
+
