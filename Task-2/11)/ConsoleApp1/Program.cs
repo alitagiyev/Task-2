bd278dc40ee2e21a68c5cd03f3e87864619278c3
alitagiyev <alint@code.edu.az>
2023-03-21 17:49:24 +0400

    done

---
+++ Task-2/11)/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 11) 8 reqemli ededin reqemlerini iki -iki qruplashdir.
+            // Qruplarin cemini tap.Alinan cavabin axirina 99 artir.
+            // Sonra cavabin ozunden onun 18 % ni cix;
+
+            int number = 12345678;
+            int cem = 0;
+
+            while (number > 0)
+            {
+                cem = cem +(number%100);
+                number /= 100;
+            }
+            Console.WriteLine($"{cem}99");
+        }
+    }
+}
