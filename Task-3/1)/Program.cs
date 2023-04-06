4d7ba87cdf0be77855f8a25c66aef68713245a19
alitagiyev <alint@code.edu.az>
2023-04-06 11:18:18 +0400

    Task 3 tamamlandi

--- Task-3/1)/Program.cs
+++ Task-3/1)/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 namespace _1_
 {
@@ -6,21 +7,32 @@ namespace _1_
     {
         static void Main(string[] args)
         {
-            string name = "";
+            int number;
+            l1:
+            Console.Write("Nece telebe daxil olsun:");
+            if (!int.TryParse(Console.ReadLine(),out number))
+            {
+                Console.WriteLine( "Reqem daxil et!!" );
+                goto l1;
+            }
+            Student[] student1 = new Student[number];
 
-            string surname = "";
-
-            string age = "";
-
-            string speciality = "";
-
-            string group = "";
-
+            for (int i = 0; i < student1.Length; i++)
+            {
+                Student student = new Student();
+                student.Name = Student.Helper($"{i + 1}.Telebenin adi:", false);
+                student.Surname = Student.Helper($"{i + 1}.Telebenin soyadi:", false);
+                student.Age = Student.Helper1($"{i + 1}.Telebenin yasi:");
+                student.Speciality = Student.Helper($"{i + 1}.Telebenin ixtisasi:", false);
+                student.GroupNo = Student.Helper($"{i + 1}.Telebenin Qrup nomresi:", false);
+                student1[i] = student;
+            }
+           Console.Clear();
+            foreach (var item in student1)
+            {
+                Console.WriteLine($"Adi:{item.Name} Soyad:{item.Surname}  Yasi:{item.Age}  Ixdisas:{item.Speciality}  Qrupu:{item.GroupNo}");
+            }
         }
-        Console.Write(name);
-        Console.Write(surname);
-        Console.Write(age);
-        Console.Write(speciality);
-        Console.Write(group);
+
     }
 }
---
+++ Task-3/1)/Student.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_
+{
+    internal class Student
+    {
+
+        public string Name;
+        public string Surname;
+        public int Age;
+        public string Speciality;
+        public string GroupNo;
+        
+
+        public static string Helper(string Add,bool allowEmpty)
+
+        {   
+            l1:
+            Console.Write(Add);
+            string name =Console.ReadLine();
+
+            if (!allowEmpty&&string.IsNullOrWhiteSpace(name))
+            {
+                goto l1;
+            }
+            return name;
+        }
+        public static int Helper1(string Add) 
+        {
+            int number;
+           l2: Console.Write(Add);
+            if (!int.TryParse(Console.ReadLine(),out number))
+            {
+                Console.WriteLine( "Reqem daxil edin:");
+                goto l2;
+            }
+            return number;  
+        }
+
+    }
+
+}
